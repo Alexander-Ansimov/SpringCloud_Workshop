c309638900fb7ed5643d496190d9ae6f1efd3022
Ansimov_Alexander <alexander.ansimov@chisw.com>
2017-11-28 13:26:59 +0200

    hystrix

--- HystrixDashboard/src/main/java/com/cloud/test/Dashboard.java
+++ HystrixDashboard/src/main/java/com/cloud/test/Dashboard.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableHystrixDashboard
+@EnableTurbine
 public class Dashboard {
     public static void main(String[] args) {
         SpringApplication.run(Dashboard.class, args);
