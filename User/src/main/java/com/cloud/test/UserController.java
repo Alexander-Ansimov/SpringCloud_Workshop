7ae5ffe84ba9b909de36c67fe03ee9c9804bbde7
Ansimov_Alexander <alexander.ansimov@chisw.com>
2017-12-20 14:22:22 +0200

    refactor

--- User/src/main/java/com/cloud/test/UserController.java
+++ User/src/main/java/com/cloud/test/UserController.java
@@ -1,5 +1,6 @@
 package com.cloud.test;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ public class UserController {
     @Autowired
     private Client client;
 
+    @HystrixCommand
     @GetMapping("/")
     public String pingUser() {
         return client.goToShop();
